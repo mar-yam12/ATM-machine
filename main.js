694732e0ea85dceb560416eb323c921a903b6e4a
Maryam Shahid <160435628+mar-yam12@users.noreply.github.com>
2024-07-05 21:47:34 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,56 @@
+// Start code by import inquirer
+import inquirer from "inquirer";
+let user = await inquirer.prompt([
+    {
+        type: "string",
+        name: "cardholdername",
+        message: " Welcome Maryam Yousuf"
+    },
+    {
+        type: "number",
+        name: "pinCode",
+        message: "Enter your 4 digit pinCode"
+    },
+    // creating listing and withdrawl method
+    {
+        type: "list",
+        name: "accountType",
+        message: " Select your account type",
+        choices: ["Current", "Saving"]
+    },
+    // Transition type
+    {
+        type: "list",
+        name: "transitionType",
+        message: "Select your transitin type",
+        choices: ["Cash", "Withdrawl"]
+    },
+    // select amount
+    {
+        type: "list",
+        name: "amount",
+        message: " Select your amount",
+        choices: [1000, 2000, 5000, 10000],
+        when(user) {
+            return user.transitionType === "Cash";
+        }
+    },
+    {
+        type: "number",
+        name: "amount",
+        message: "Enter your amount",
+        when(user) {
+            return user.transitionType === "Withdrawl";
+        }
+    }
+]);
+// Final Slip
+if (user.pinCode) {
+    const balance = Math.floor(Math.random() * 10000);
+    console.log(balance);
+    const enteramount = user.account;
+    if (enteramount >= balance) {
+        const remaining = balance - enteramount;
+        console.log(`you have remaining balance is ${enteramount} and you have remaining balance is ${remaining}`);
+    }
+}
